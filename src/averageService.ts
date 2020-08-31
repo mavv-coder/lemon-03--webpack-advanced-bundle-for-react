4fd00e32d73a6dc1df2b680adf8dc83d6dea12eb
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-31 10:48:58 +0200

    environment variables working

--- src/averageService.ts
+++ src/averageService.ts
@@ -5,3 +5,5 @@ export function getAvg(scores: number[]) {
 export function getTotalScore(scores: number[]) {
   return scores.reduce((score, count) => score + count);
 }
+
+console.log(`Api base: ${process.env.API_BASE}`);
